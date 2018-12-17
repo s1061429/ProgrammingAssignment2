dcab9f36e91f0645dc857e52f7b38e737ef9b657
MJ Menkhorst <mjh_menkhorst@studentsplus.nl>
2018-12-17 16:28:21 +0100

    Result 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## This function makes a special case matrix object. It will create the possibility to add some additional 
+## information to the matrix x
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
         setMatrix <- function(y) {
@@ -18,9 +18,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
-## Write a short comment describing this function
-
+## This function calculates the inverse of the special case matrix object. Is this object already has a 
+## inverse calculated, it will return this.
 cacheSolve <- function(x, ...) {
         i <- x$getInverse()
         if(!is.null(i)) {
